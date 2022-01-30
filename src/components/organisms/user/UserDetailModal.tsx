3b2f0a144ff9d0357157392d1641ad04b2026c33
nmackee <70114875+nmackee@users.noreply.github.com>
2022-01-30 16:15:12 +0900

    ユーザー詳細モーダル画面の作成

---
+++ src/components/organisms/user/UserDetailModal.tsx
@@ -0,0 +1,58 @@
+import { memo, VFC } from "react";
+import {
+  FormLabel,
+  Input,
+  isOpen,
+  onClose,
+  FormControl,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalHeader,
+  ModalOverlay,
+  Stack
+} from "@chakra-ui/react";
+
+type Props = {
+  isOpen: boolean;
+  onClose: () => void;
+};
+
+export const UserDetailModal: VFC<Props> = memo((props) => {
+  const { isOpen, onClose } = props;
+  return (
+    <Modal
+      isOpen={isOpen}
+      onClose={onClose}
+      autoFocus={false}
+      motionPreset="slideInBottom"
+    >
+      <ModalOverlay />
+      <ModalContent pb={6}>
+        <ModalHeader>ユーザー詳細</ModalHeader>
+        <ModalCloseButton />
+        <ModalBody mx={4}>
+          <Stack spacing={4}>
+            <FormControl>
+              <FormLabel>名前</FormLabel>
+              <Input value="じゃけえ" isReadOnly />
+            </FormControl>
+            <FormControl>
+              <FormLabel>フルネーム</FormLabel>
+              <Input value="Takuya Okada" isReadOnly />
+            </FormControl>
+            <FormControl>
+              <FormLabel>MAIL</FormLabel>
+              <Input value="1234@sample.com" isReadOnly />
+            </FormControl>
+            <FormControl>
+              <FormLabel>TEL</FormLabel>
+              <Input value="090-1111-2222" isReadOnly />
+            </FormControl>
+          </Stack>
+        </ModalBody>
+      </ModalContent>
+    </Modal>
+  );
+});
--- src/components/pages/UserManagement.tsx
+++ src/components/pages/UserManagement.tsx
@@ -1,12 +1,22 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import { Center, Spinner, Wrap, WrapItem } from "@chakra-ui/react";
-import { memo, useEffect, VFC } from "react";
+import {
+  useDisclosure,
+  Center,
+  Spinner,
+  Wrap,
+  WrapItem
+} from "@chakra-ui/react";
+import { memo, useCallback, useEffect, VFC } from "react";
 
 import { UserCard } from "../organisms/user/UserCard";
+import { UserDetailModal } from "../organisms/user/UserDetailModal";
 import { useAllUsers } from "../../hooks/useAllUsers";
 
 export const UserManagement: VFC = memo(() => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
   const { getUsers, users, loading } = useAllUsers();
+
+  const onClickUser = useCallback(() => onOpen(), []);
 
   useEffect(() => getUsers(), []);
 
@@ -24,11 +34,13 @@ export const UserManagement: VFC = memo(() => {
                 imageUrl="https://source.unsplash.com/random"
                 userName={user.username}
                 fullName={user.name}
+                onClick={onClickUser}
               />
             </WrapItem>
           ))}
         </Wrap>
       )}
+      <UserDetailModal isOpen={isOpen} onClose={onClose} />
     </>
   );
 });
